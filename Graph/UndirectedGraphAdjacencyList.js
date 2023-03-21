6669e6c4c9eb1188ad98d234c4e410f0987e8dde
hyununam <sksskdfg@gmail.com>
2023-03-21 13:09:08 +0900

    2022-03-21

---
+++ Graph/UndirectedGraphAdjacencyList.js
@@ -0,0 +1,41 @@
+class Graph {
+    constructor(vertices) {
+        this.vertices = vertices;
+        this.adjList = new Map();
+    }
+
+    addVertex(v) {
+        this.adjList.set(v, []);
+    }
+
+    addEdge(v, w) {
+        this.adjList.get(v).push(w);
+        this.adjList.get(w).push(v);
+    }
+
+    printGraph() {
+        let keys = this.adjList.keys();
+
+        for (let key of keys) {
+            const values = this.adjList.get(key).values();
+            let txt = '';
+            for (let value of values) {
+                txt += value + ' ';
+            }
+            console.log(key, '->', txt);
+        }
+    }
+}
+
+const g = new Graph(5);
+const vertices = [1, 2, 3, 4, 5];
+
+vertices.forEach(e => g.addVertex(e));
+// for (let v of vertices) {
+//     g.addVertex(v);
+// }
+
+g.addEdge(1, 2);
+g.addEdge(3, 5);
+
+g.printGraph();
