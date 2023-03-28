862257cd4ebd4025fb141a43dc36545a73f81803
hyununam <sksskdfg@gmail.com>
2023-03-28 14:39:38 +0900

    2023-03-28

---
+++ Sort/InsertionSort/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+
+given_arr = [5, 2, 1, 9, 7]
+insertion_sort(given_arr)
+print(given_arr)
